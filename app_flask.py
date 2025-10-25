5a878c078681d5a30640dad72963f3d4cd75eee3
Nery008 <gabrielleneryslima@gmail.com>
2025-10-25 09:11:06 -0300

    Implement arithmetic operation routes in Flask app
    Added routes for arithmetic operations: addition, subtraction, multiplication, and division.

--- app_flask.py
+++ app_flask.py
@@ -2,12 +2,37 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def index():
     return render_template("index.html")
 
-## Continue o código aqui.
+# Continue o código aqui.
+@app.route('/soma')
+def soma():
+    v1 = float(request.args.get('valor1', 0))
+    v2 = float(request.args.get('valor2', 0))
+    return {'resultado': v1 + v2}
+
+@app.route('/subtracao')
+def subtracao():
+    v1 = float(request.args.get('valor1', 0))
+    v2 = float(request.args.get('valor2', 0))
+    return {'resultado': v1 - v2}
+
+@app.route('/multiplicacao')
+def multiplicacao():
+    v1 = float(request.args.get('valor1', 0))
+    v2 = float(request.args.get('valor2', 0))
+    return {'resultado': v1 * v2}
+
+@app.route('/dividir')
+def dividir():
+    v1 = float(request.args.get('valor1', 0))
+    v2 = float(request.args.get('valor2', 0))
+    if v2 == 0:
+        return{'erro':'Valor inesistente'}
+    return{'resultado': v1/v2}
+
 
 if __name__ == "__main__":
     app.run(debug=True)
