e4b91b67e60838098e469b32f178b5d4bfdf8de1
Nery008 <gabrielleneryslima@gmail.com>
2025-10-25 11:00:09 -0300

    Update app_sqlite.py

--- app_sqlite.py
+++ app_sqlite.py
@@ -12,3 +12,47 @@ O que o script deve fazer:
 7) Deletar registros
 
 """
+
+
+import sqlite3
+conn= sqlite3.connect('escola.db')
+cursor = conn.cursor()
+
+cursor.execute('''CREATE TABLE IF NOT EXISTS alunos(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL,
+    idade INTEGER,
+    email TEXT
+    )''')
+
+print('Tabela criada com sucesso!\n')
+
+cursor.execute('INSERT INTO alunos(nome,idade,email) VALUES(?,?,?)',('Bia',19,'beatriz@gmail.com'))
+cursor.execute('INSERT INTO alunos(nome,idade,email) VALUES(?,?,?)',('Cauã',19,'cauaaa@gmail.com'))
+cursor.execute('INSERT INTO alunos(nome,idade,email) VALUES(?,?,?)',('Gustavo',18,'gusta@gmail.com'))
+conn.commit()
+print('Dados completos!\n')
+
+
+print('Lista de alunos cadastrados:')
+cursor.execute('SELECT * FROM alunos')
+for linha in cursor.fetchall():
+    print(linha)
+print()
+
+cursor.execute('UPDATE alunos SET email = ? WHERE nome = ?',('caua2002@gmail.com','Cauã'))
+print('após atualização do email do Cauã')
+cursor.execute('SELECT * FROM alunos')
+for linha in cursor.fetchall():
+    print(linha)
+print()
+
+cursor.execute('DELETE FROM alunos WHERE id= ?',(2,))
+conn.commit()
+print('Após deletar o id 1:')
+cursor.execute('SELECT * FROM alunos')
+for linha in cursor.fetchall():
+    print(linha)
+print()
+
+conn.close()
